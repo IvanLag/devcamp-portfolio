648588b7074fae7a8482153192ba2d579199a5b3
Ivan Lagutin <ivan.lag95@gmail.com>
2023-08-13 14:19:24 +0400

    Fixed portfolios column name

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,7 +9,7 @@ class PortfoliosController < ApplicationController
 
 	def create
 
-	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:titel, :sublitel, :body))
+	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
 	    respond_to do |format|
 	      if @portfolio_item.save
@@ -27,7 +27,7 @@ class PortfoliosController < ApplicationController
   def update
 	@portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:titel, :sublitel, :body))
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_100005) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_101228) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_100005) do
   end
 
   create_table "portfolios", force: :cascade do |t|
-    t.string "titel"
-    t.string "sublitel"
+    t.string "title"
+    t.string "subtitle"
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
