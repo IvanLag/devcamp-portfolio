4a0687d2895fdc4cc68308d6d06f71bd71546478
Ivan Lagutin <ivan.lag95@gmail.com>
2023-09-18 22:53:48 +0400

    Installed jquery ui, sortable html5 library and css calls

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require jquery3
 //= require bootstrap
+//= require jquery-ui
+//= require html.sortable
