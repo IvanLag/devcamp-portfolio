b64625cc7c0d42a00357a24e98afa5050f8d1032
Ivan Lagutin <ivan.lag95@gmail.com>
2023-11-13 22:07:59 +0400

    Integrated custom fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,7 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( blogs.css )
 Rails.application.config.assets.precompile += %w( portfolios.css )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.js popper.js)
 
